73a0a3eb27c0b82eca54723cda93566944917ff2
Sankeerthanasuddala <127500161+Sankeerthanasuddala@users.noreply.github.com>
2023-03-25 17:34:11 +0530

    division feature added

--- area.py
+++ area.py
@@ -1,6 +1,9 @@
 def addition(a,b):
     return(a+b)
+def division(a):
+    return(a/2)
 #def division:
 a=int(input("Enter any number :"))
 b=int(input("Enter any number :"))
 sum=addition(a,b)
+b=division(sum)
