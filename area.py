0bf24bcefde2b5ed62ac9618b46e702ca152f5b4
Sankeerthanasuddala <127500161+Sankeerthanasuddala@users.noreply.github.com>
2023-03-25 18:18:21 +0530

    multiplication feature added

--- area.py
+++ area.py
@@ -1,9 +1,13 @@
 def addition(a,b):
     return(a+b)
-def division(a):
-    return(a/2)
-#def division:
-a=int(input("Enter any number :"))
-b=int(input("Enter any number :"))
+def division(sum):
+    return(sum/2)
+def multiplication(div,h):
+    return(div*h)
+a=int(input("Enter any number"))
+b=int(input("Enter any number"))
+h=int(input("Enter height"))
 sum=addition(a,b)
-b=division(sum)
+div=division(sum)
+area=multiplication(div,h)
+print(area)
